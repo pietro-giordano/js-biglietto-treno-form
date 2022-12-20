8e9a4050c9613f122ea4df1f946d15fbb73b6698
Pietro Giordano <giopi87@gmail.com>
2022-12-20 18:12:24 +0100

    avanzamento

--- js/scripts.js
+++ js/scripts.js
@@ -9,36 +9,55 @@ const cabOutput = document.getElementById('cab-output');
 const cpOutput = document.getElementById('cp-output');
 const priceOutput = document.getElementById('price-output');
 
-let priceTicket = kmInput * 0.21;
-let priceTicketUnder = priceTicket - (priceTicket * 0.8);
-let priceTicketOver = priceTicket - (priceTicket * 0.6);
-
 submitButton.addEventListener('click',
 
       function() {
 
+            let priceTicket = parseInt(kmInput.value * 0.21);
+            let priceTicketUnder = priceTicket - (priceTicket * 0.8);
+            let priceTicketOver = priceTicket - (priceTicket * 0.6);
+
             console.log('Cliccato su submitButton');
             console.log('Il valore di nameInput è', nameInput.value);
             console.log('Il valore di kmInput è', kmInput.value);
+            console.log(typeof kmInput);
             console.log('Il valore di ageSelect è', ageSelect.value);
 
             if (ageSelect.value == 'under') {
                   nameOutput.innerHTML = nameInput.value;
                   discountOutput.innerHTML = 'Sconto Minorenni';
-                  cabOutput.innerHTML = 'Sconto Minorenni';
-                  cpOutput.innerHTML = 'Sconto Minorenni';
-                  priceOutput.innerHTML = 'Sconto Minorenni';
+                  cabOutput.innerHTML = Math.floor(Math.random() * 10);
+                  cpOutput.innerHTML = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
+                  priceOutput.innerHTML = priceTicketUnder.toFixed(2) + '€';
             }
             else if (ageSelect.value == 'over') {
-                  answer.innerHTML = 'L\'utente ' + nameInput.value + ' ha indicato come genere Donna';
+                  nameOutput.innerHTML = nameInput.value;
+                  discountOutput.innerHTML = 'Sconto Over 65';
+                  cabOutput.innerHTML = Math.floor(Math.random() * 10);
+                  cpOutput.innerHTML = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
+                  priceOutput.innerHTML = priceTicketOver.toFixed(2) + '€';
             }
             else {
-                  answer.innerHTML = 'L\'utente ' + nameInput.value + ' ha preferito non specificare il proprio genere';
+                  nameOutput.innerHTML = nameInput.value;
+                  discountOutput.innerHTML = 'Biglietto Standard';
+                  cabOutput.innerHTML = Math.floor(Math.random() * 10);
+                  cpOutput.innerHTML = Math.floor(Math.random() * (99999 - 90000 + 1)) + 90000;
+                  priceOutput.innerHTML = priceTicket.toFixed(2) + '€';
             }
 
       }
 
 )
+
+// resetButton.addEventListener('click',
+
+//       function() {
+
+//             document.getElementById("my_form").reset(); 
+
+//       }
+
+// )
 
 
 // if (age < 18) {
